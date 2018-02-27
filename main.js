aca738062e03d0852bbb2abfba4aac881ac0d005
Nabil Hassein <nabil.hassein@gmail.com>
2018-02-27 17:00:47 -0500

    almost there

--- main.js
+++ main.js
@@ -4,26 +4,12 @@ var lines = [];
 p5 noise applied to an imitation of vera molnar's work; cribbed from:
 https://codepen.io/enxaneta/pen/7e315d161a8ee073ded48ab5d1669290
 */
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var cw = canvas.width = 600;
-var ch = canvas.height = 600;
-ctx.lineJoin = "round";
-ctx.strokeStyle = "#fff";
-ctx.fillStyle = "rgba(0,0,0,1)";
 
 var squares = [];
 
-function map(n, a, b, _a, _b) {
-  var d = b - a;
-  var _d = _b - _a;
-  var u = _d / d;
-  return _a + (n - a) * u;
-}
-
-
 function setup() {
-    for (var s = 0; s < cw; s += 10) {
+    createCanvas(800, 800);
+    for (var s = 0; s < width; s += 10) {
         var square = new SquigglySquare(s);
         squares.push(square);
     }
@@ -31,84 +17,47 @@ function setup() {
 
 
 function draw() {
-    requestId = window.requestAnimationFrame(draw);
-
-    ctx.fillRect(0, 0, cw, ch);
+    background(0);
     noiseDetail(2, .5);
 
     for (var i = 0; i < squares.length; i++) {
-        squares[i].phi += 1 / 30;
         squares[i].draw();
     }
 }
 
 function SquigglySquare(len) {
-    var amplitude = 5;
-    var frequency = .02;
-    var rad = Math.PI / 180;
-
-    this.x = (cw - len)/2;
-    this.y = (ch - len)/2;
-
-    this.xoff = Math.random() * 10000;
-    this.Xoff = this.xoff;
-    this.phi = Math.random() * 10000;
-
-    function squigglyHorizontal(height, xstart, xend) {
-        ctx.beginPath();
-        this.xoff = this.Xoff;
-
-        ctx.moveTo(xstart, height);
+    function squigglyHorizontal(column, xstart, xend) {
+        noFill();
+        stroke(255);
+        beginShape();
 
         for (var x = xstart; x < xend; x++) {
-            if (x > cw / 3 && x < 2 * cw / 3) {
-                var k = map(x, cw / 3, 2 * cw / 3, 0, 180);
-            } else {
-                k = 0;
-            }
-
-            var y = -Math.abs(Math.sin((x + noise(this.xoff) * 100) * frequency + this.phi) * (amplitude + Math.sin(k * rad) * 50)) + this.y;
-
-            ctx.lineTo(x, y);
-
-            this.xoff += 0.05;
+            var y = noise(x + Math.random(), frameCount) + column;
+            curveVertex(x, y);
         }
 
-        ctx.lineTo(xend, height);
+        endShape();
+    }
 
-        ctx.closePath();
-        ctx.fill();
-        ctx.stroke();
+    function squigglyVertical(row, ystart, yend) {
+        noFill();
+        stroke(255);
+        beginShape();
+
+        for (var y = ystart; y < yend; y++) {
+            var x = noise(y + Math.random(), frameCount) + row;
+            curveVertex(x, y);
+        }
+
+        endShape();
     }
 
     this.draw = function() {
-        var x = this.x;
-        var y = this.y;
+        var x = (width - len)/2;
+        var y = (height - len)/2;
         squigglyHorizontal(y, x - len, x + len);
-        // squigglyHorizontal(y + len, x - len, x + len);
-        // ctx.beginPath();
-
-        // this.xoff = this.Xoff; // reset xoff;
-
-        // var x = this.x;
-        // var y = -Math.abs(Math.sin((x + noise(this.xoff) * 100) * frequency + this.phi) * amplitude) + this.y;
-
-        // ctx.moveTo(x, y);
-        // ctx.lineTo(x, y+len);
-        // ctx.stroke();
-
-        // ctx.moveTo(x, y);
-        // ctx.lineTo(x+len, y);
-        // ctx.stroke();
-
-        // ctx.moveTo(x+len, y);
-        // ctx.lineTo(x+len, y+len);
-        // ctx.stroke();
-        
-        // ctx.moveTo(x, y+len);
-        // ctx.lineTo(x+len, y+len);
-        // ctx.stroke();
+        squigglyHorizontal(y + len, x - len, x + len);
+        squigglyVertical(x, y - len, y + len);
+        squigglyVertical(x + len, y - len, y + len);
     }
 }
-
-requestId = window.requestAnimationFrame(draw);
